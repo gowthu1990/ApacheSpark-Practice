f57a893d8ea3e61de2d659d11a99da23ec4e1fa8
Sai Gowtham Badvity <gowtham.badvity@gmail.com>
2021-03-06 16:14:55 +0000

    Initial commit

---
+++ src/main/scala/com/clearurdoubt/DatasetTest.scala
@@ -0,0 +1,27 @@
+package com.clearurdoubt
+
+import org.apache.spark.sql.SparkSession
+
+object DatasetTest extends App {
+
+  val spark = SparkSession.builder
+    .appName("Spark SQL - First App")
+    .master("local[*]")
+    .getOrCreate()
+
+  val ds = spark.read.textFile("src/main/resources/README.md")
+  ds.show(truncate = false)
+
+  val firstLine: String = ds.first
+  println("firstLine: " + firstLine)
+
+  val sparkLines = ds.filter(line => line.contains("Spark"))
+  sparkLines.show(truncate = false)
+
+  sparkLines.write
+    .format("text")
+    .save("target/textOutput")
+
+  spark.stop()
+}
+
